f798b02856b6792ba8ed7398e80d1263ace00b8a
Amro <amroqadadha@gmail.com>
2024-11-01 16:40:31 +0200

    Add publisher class to decouble dependencies

---
+++ ServerMonitoringSystem/ServerStatisticsPublisher.cs
@@ -0,0 +1,17 @@
+﻿namespace ServerMonitoringSystem
+{
+    public class ServerStatisticsPublisher
+    {
+        private IMessageQueue MessageQueue { get; set; }
+
+        public ServerStatisticsPublisher(IMessageQueue messageQueue)
+        {
+            MessageQueue = messageQueue;
+        }
+
+        public void PublishServerStatistics(string topic, ServerStatistics serverStatistics)
+        {
+            MessageQueue.Publish(topic, serverStatistics.ToString());
+        }
+    }
+}
