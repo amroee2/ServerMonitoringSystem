026cf2ab29ab369bca9d8c10b1a0c93ca4b5953a
Amro <amroqadadha@gmail.com>
2024-11-01 13:12:22 +0200

    Create Server Statistics Repository

---
+++ ServerMonitoringSystem/ServerStatisticsRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+
+namespace ServerMonitoringSystem
+{
+    public class ServerStatisticsRepository
+    {
+        private ServerStatistics ServerStatistics { get; set; }
+        private static PerformanceCounter CpuCounter = new("Processor", "% Processor Time", "_Total");
+        private static PerformanceCounter RamCounter = new("Memory", "Available MBytes");
+        private static PerformanceCounter TotalMemoryCounter = new("Memory", "Committed Bytes");
+
+        public ServerStatisticsRepository()
+        {
+            ServerStatistics = new ServerStatistics();
+        }
+        public void UpdateStatistics()
+        {
+            ServerStatistics.MemoryUsage = GetMemoryUsage();
+            ServerStatistics.AvailableMemory = GetAvailableMemory();
+            ServerStatistics.CpuUsage = GetCpuUsage();
+            ServerStatistics.Timestamp = DateTime.Now;
+        }
+
+        public double GetMemoryUsage()
+        {
+            double totalMemory = TotalMemoryCounter.NextValue() / (1024 * 1024);
+            double availableMemory = GetAvailableMemory();
+            double memoryUsage = totalMemory - availableMemory;
+            return memoryUsage;
+        }
+
+        public double GetAvailableMemory()
+        {
+            return RamCounter.NextValue();
+        }
+
+        public double GetCpuUsage()
+        {
+            return CpuCounter.NextValue();
+        }
+
+        public void ShowServerStatistics()
+        {
+            Console.WriteLine(ServerStatistics);
+        }
+    }
+}
