d598541c82c185c15fc96207213cf9d6d26d93f6
Amro <amroqadadha@gmail.com>
2024-11-01 13:27:04 +0200

    Create data model for configurations

---
+++ ServerMonitoringSystem/ServerStatisticsConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace ServerMonitoringSystem
+{
+    public class ServerStatisticsConfig
+    {
+        public int SamplingIntervalSeconds { get; set; }
+        public string ServerIdentifier { get; set; }
+    }
+}
