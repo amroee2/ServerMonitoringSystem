d55daa5913d3e6431007bdaa4b439107bd1f527d
Amro <amroqadadha@gmail.com>
2024-11-02 12:45:56 +0200

    Implement and Refactor Program class
    Organized by being separated into different methods and implemented SiganlR Server and Client for anamoly detection

--- ServerMonitoringSystem/Program.cs
+++ ServerMonitoringSystem/Program.cs
@@ -1,31 +1,95 @@
 ﻿using Newtonsoft.Json.Linq;
 using ServerMonitoringSystem;
+using ServerMonitoringSystem.DatabaseManagement;
 using ServerMonitoringSystem.MessageQueueServices;
 using ServerMonitoringSystem.ServerStatisticsManagement;
-using System.Text.Json;
+using ServerMonitoringSystem.SignalRManagement;
 
-ServerStatisticsRepository serverStatisticsRepository = new();
-ServerStatisticsConfig config = ReadConfig("appsettings.json");
-ServerStatisticsPublisher serverStatisticsPublisher = new(new RabbitMQService(new MongoDBRepository()));
-ServerStatistics statistics = new();
-while (true)
+await StartServerMonitoringAsync();
+
+async Task StartServerMonitoringAsync()
 {
-    statistics = serverStatisticsRepository.UpdateStatistics();
-    statistics.ServerIdentifier = config.ServerIdentifier;
-    serverStatisticsPublisher.PublishServerStatistics(statistics);
-    serverStatisticsPublisher.GetMessage();
-    Thread.Sleep(config.SamplingIntervalSeconds * 1000);
+    var server = new SignalRServer();
+    await server.StartAsync();
+
+    var client = new SignalRClient();
+    await client.ConnectAsync();
+
+    var config = LoadServerStatisticsConfig("appsettings.json");
+    var anamolyThresholdConfig = LoadAnamolyThresholdConfig("appsettings.json");
+
+    var mongoDBRepository = new MongoDBRepository();
+    DatabaseRepository databaseRepository = new DatabaseRepository(mongoDBRepository);
+    var serverStatisticsRepository = new ServerStatisticsRepository();
+    var serverStatisticsPublisher = new ServerStatisticsPublisher(new RabbitMQService());
+    var anamolyDetectionRepository = new AnamolyDetectionRepository(anamolyThresholdConfig, client);
+
+    await RunStatisticsMonitoringLoopAsync(config, serverStatisticsRepository, serverStatisticsPublisher, databaseRepository, anamolyDetectionRepository);
 }
 
-static ServerStatisticsConfig ReadConfig(string filePath)
+async Task RunStatisticsMonitoringLoopAsync(ServerStatisticsConfig config, ServerStatisticsRepository serverStatisticsRepository, ServerStatisticsPublisher serverStatisticsPublisher,
+    DatabaseRepository databaseRepository, AnamolyDetectionRepository anamolyDetectionRepository)
+{
+    int iterations = 0;
+
+    while (true)
+    {
+        var statistics = serverStatisticsRepository.UpdateStatistics();
+        statistics.ServerIdentifier = config.ServerIdentifier;
+
+        serverStatisticsPublisher.PublishServerStatistics(statistics);
+
+        if (iterations == 0)
+        {
+            await InitialStatisticsSetupAsync(statistics, config, databaseRepository);
+            iterations++;
+            continue;
+        }
+
+        iterations++;
+        await PerformStatisticsAndAnomalyDetectionAsync(statistics, serverStatisticsPublisher, databaseRepository, anamolyDetectionRepository);
+        Thread.Sleep(config.SamplingIntervalSeconds * 1000);
+    }
+}
+
+async Task InitialStatisticsSetupAsync(ServerStatistics statistics, ServerStatisticsConfig config, DatabaseRepository databaseRepository)
+{
+    Thread.Sleep(config.SamplingIntervalSeconds * 1000);
+    await databaseRepository.InsertDocumentAsync(statistics);
+}
+
+async Task PerformStatisticsAndAnomalyDetectionAsync(ServerStatistics statistics, ServerStatisticsPublisher serverStatisticsPublisher,
+    DatabaseRepository databaseRepository, AnamolyDetectionRepository anamolyDetectionRepository)
+{
+    var previousStatistics = await databaseRepository.GetLatestDocumentAsync();
+    var latestStatistics = serverStatisticsPublisher.GetMessage();
+
+    anamolyDetectionRepository.DetectAnamoly(statistics, previousStatistics);
+    anamolyDetectionRepository.DetectHighUsage(statistics, previousStatistics);
+
+    await databaseRepository.InsertDocumentAsync(statistics);
+}
+
+ServerStatisticsConfig LoadServerStatisticsConfig(string filePath)
 {
     JObject config = JObject.Parse(File.ReadAllText(filePath));
-    int samplingIntervalSeconds = (int)config["ServerStatisticsConfig"]["SamplingIntervalSeconds"];
-    string serverIdentifier = (string)config["ServerStatisticsConfig"]["ServerIdentifier"];
 
     return new ServerStatisticsConfig
     {
-        SamplingIntervalSeconds = samplingIntervalSeconds,
-        ServerIdentifier = serverIdentifier
+        SamplingIntervalSeconds = (int)config["ServerStatisticsConfig"]["SamplingIntervalSeconds"],
+        ServerIdentifier = (string)config["ServerStatisticsConfig"]["ServerIdentifier"]
+    };
+}
+
+AnamolyThresholdConfig LoadAnamolyThresholdConfig(string filePath)
+{
+    JObject config = JObject.Parse(File.ReadAllText(filePath));
+
+    return new AnamolyThresholdConfig
+    {
+        CpuUsageAnomalyThresholdPercentage = (double)config["AnomalyDetectionConfig"]["CpuUsageAnomalyThresholdPercentage"],
+        MemoryUsageAnomalyThresholdPercentage = (double)config["AnomalyDetectionConfig"]["MemoryUsageAnomalyThresholdPercentage"],
+        MemoryUsageThresholdPercentage = (double)config["AnomalyDetectionConfig"]["MemoryUsageThresholdPercentage"],
+        CpuUsageThresholdPercentage = (double)config["AnomalyDetectionConfig"]["CpuUsageThresholdPercentage"]
     };
 }
