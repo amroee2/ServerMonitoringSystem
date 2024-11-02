42ee187445500370e54ae8917e44225a72f6a433
Amro <amroqadadha@gmail.com>
2024-11-02 12:42:43 +0200

    Add database repository class

---
+++ ServerMonitoringSystem/DatabaseManagement/DatabaseRepository.cs
@@ -0,0 +1,24 @@
+﻿using ServerMonitoringSystem.ServerStatisticsManagement;
+
+namespace ServerMonitoringSystem.DatabaseManagement
+{
+    public class DatabaseRepository
+    {
+
+        private readonly IDatabaseRepository _databaseRepository;
+        public DatabaseRepository(IDatabaseRepository databaseRepository)
+        {
+            _databaseRepository = databaseRepository;
+        }
+
+        public async Task InsertDocumentAsync(ServerStatistics document)
+        {
+            await _databaseRepository.InsertDocumentAsync(document);
+        }
+
+        public async Task<ServerStatistics> GetLatestDocumentAsync()
+        {
+            return await _databaseRepository.GetLatestDocumentAsync();
+        }
+    }
+}
