6843b7796f9fa7f08833a0c9ab64cfc940cc1c6e
Amro <amroqadadha@gmail.com>
2024-11-02 12:44:46 +0200

    Implement anamoly detection class

---
+++ ServerMonitoringSystem/AnamolyThresholdConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace ServerMonitoringSystem
+{
+    public class AnamolyThresholdConfig
+    {
+        public double MemoryUsageAnomalyThresholdPercentage = 0.4;
+        public double CpuUsageAnomalyThresholdPercentage = 0.5;
+        public double MemoryUsageThresholdPercentage = 0.8;
+        public double CpuUsageThresholdPercentage = 0.9;
+    }
+}
